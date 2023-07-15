281c7436455a1f0cf182763600bec37b7b57c32e
unknown <correo>
2023-07-14 18:42:05 -0600

    hola profe regaleme puntos

--- proyectoBIM3-Erick-Lemus/src/proyectobim3/erick/lemus/ProyectoBIM3ErickLemus.java
+++ proyectoBIM3-Erick-Lemus/src/proyectobim3/erick/lemus/ProyectoBIM3ErickLemus.java
@@ -1,12 +1,32 @@
 
 package proyectobim3.erick.lemus;
 
+import java.util.Scanner;
+
 
 public class ProyectoBIM3ErickLemus {
-
+ static contacto contactos[] = new contacto[15];
+    static int num = 0;
     
     public static void main(String[] args) {
-        
-    }
+      
+
     
+
+        int op = 0;
+        Scanner teclado = new Scanner(System.in);
+
+        while (op != 5) {
+            System.out.println("--------------------");
+            System.out.println("       MENU      ");
+            System.out.println("1. Guardar Contacto");
+            System.out.println("2. Ver Todos los Contactos");
+            System.out.println("3. Buscar un Contacto");
+            System.out.println("4. Actualizar Contacto");
+            System.out.println("5. Salir");
+            System.out.println("--------------------");
+            System.out.println("Ingrese el Numero de Opcion");
+            op = teclado.nextInt();
+        }
+    }
 }
--- proyectoBIM3-Erick-Lemus/src/proyectobim3/erick/lemus/contacto.java
+++ proyectoBIM3-Erick-Lemus/src/proyectobim3/erick/lemus/contacto.java
@@ -3,5 +3,13 @@ package proyectobim3.erick.lemus;
 
 
 public class contacto {
-    
+    String telefono, nombre, domicilio;
+
+    public contacto(String telefono, String nombre, String domicilio) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+    }
 }
+
+   
